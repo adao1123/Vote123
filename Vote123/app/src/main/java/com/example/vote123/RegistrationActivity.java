22cfcb85f228a290a8b7a19e0dda4b8dea419d87
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-04-30 09:33:54 -0700

    Saving work to move to RegistrationActivity

--- Vote123/app/src/main/java/com/example/vote123/RegistrationActivity.java
+++ Vote123/app/src/main/java/com/example/vote123/RegistrationActivity.java
@@ -2,12 +2,19 @@ package com.example.vote123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class RegistrationActivity extends AppCompatActivity {
+    WebView webView;
+    WebViewClient webViewClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+
     }
 }
