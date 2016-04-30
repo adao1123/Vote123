04dd54ee1fb765a9dd4df71360ed3ae5b922a9b3
adao1123 <adao1193@gmail.com>
2016-04-30 11:13:42 -0700

    Views in Understanding issues actitivity

--- Vote123/app/src/main/java/com/example/vote123/UnderstandingIssuesActivity.java
+++ Vote123/app/src/main/java/com/example/vote123/UnderstandingIssuesActivity.java
@@ -2,12 +2,32 @@ package com.example.vote123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class UnderstandingIssuesActivity extends AppCompatActivity {
+
+    TextView questionTV;
+    TextView costTV;
+    Button noButton;
+    Button yesButton;
+    Button idkButton;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_understanding_issues);
+        initializeViews();
+
+    }
+
+    private void initializeViews(){
+        questionTV = (TextView)findViewById(R.id.question_TV_ID);
+        costTV = (TextView)findViewById(R.id.cost_TV_ID);
+        noButton = (Button)findViewById(R.id.NO_button_ID);
+        yesButton = (Button)findViewById(R.id.YES_button_ID);
+        idkButton = (Button)findViewById(R.id.IDK_button_ID);
     }
 }
