065b33e7ebd9f8feb4b1d949eab12df5df9b1936
adao1123 <adao1193@gmail.com>
2016-04-29 22:41:29 -0700

    Added Acticities

---
+++ Vote123/app/src/main/java/com/example/vote123/FindPollingActivity.java
@@ -0,0 +1,13 @@
+package com.example.vote123;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindPollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_polling);
+    }
+}
---
+++ Vote123/app/src/main/java/com/example/vote123/QuickReferenceActivity.java
@@ -0,0 +1,13 @@
+package com.example.vote123;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuickReferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_reference);
+    }
+}
