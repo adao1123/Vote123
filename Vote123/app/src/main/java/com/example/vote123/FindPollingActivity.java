ab5acc8abc3810972bd5458a31e6bd44b54e1832
Billy <Billy@BILLs-MacBook-Pro.local>
2016-04-30 12:31:13 -0700

    made add calendar event for election day

--- Vote123/app/src/main/java/com/example/vote123/FindPollingActivity.java
+++ Vote123/app/src/main/java/com/example/vote123/FindPollingActivity.java
@@ -1,6 +1,11 @@
 package com.example.vote123;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +13,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.TimeZone;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
 
 public class FindPollingActivity extends AppCompatActivity {
 
@@ -33,12 +41,17 @@ public class FindPollingActivity extends AppCompatActivity {
     String inputedAddress;
     String returnAddress;
     CivicInfoApiService service;
+    Button calendarButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_polling);
 
+        calendarButton = (Button)findViewById(R.id.add_election_date);
         editStreet = (EditText)findViewById(R.id.editTextStreetName);
         editCity = (EditText)findViewById(R.id.editTextCity);
         editState = (EditText)findViewById(R.id.editTextState);
@@ -79,5 +92,99 @@ public class FindPollingActivity extends AppCompatActivity {
                 });
             }
         });
+
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                insertEventInCalendar("Election Day", "Don't forget to Vote today!" , returnAddress);
+                addReminderInCalendar();
+
+            }
+        });
+
     }
+
+
+    public void insertEventInCalendar(String title, String description, String location) {
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2016, Calendar.NOVEMBER, 8, 10, 0);
+        long startMillis = beginTime.getTimeInMillis();
+
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2016, Calendar.NOVEMBER, 8, 23, 0);
+        long endMillis = endTime.getTimeInMillis();
+
+
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.DESCRIPTION, description)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+        startActivity(intent);
+
+
+    }
+
+
+    /** Adds Events and Reminders in Calendar. */
+    private void addReminderInCalendar() {
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2016, Calendar.NOVEMBER, 8, 10, 0);
+        long startMillis = beginTime.getTimeInMillis();
+
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2016, Calendar.NOVEMBER, 8, 23, 0);
+        long endMillis = endTime.getTimeInMillis();
+
+
+        Calendar cal = Calendar.getInstance();
+        Uri EVENTS_URI = Uri.parse(getCalendarUriBase(true) + "events");
+        ContentResolver cr = getContentResolver();
+        TimeZone timeZone = TimeZone.getDefault();
+
+        /** Inserting an event in calendar. */
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events.CALENDAR_ID, 2);
+        values.put(CalendarContract.Events.TITLE, "Election Day");
+        values.put(CalendarContract.Events.DESCRIPTION, "Remember to Vote Today!");
+        values.put(CalendarContract.Events.ALL_DAY, 0);
+        values.put(CalendarContract.Events.DTSTART, startMillis);
+        values.put(CalendarContract.Events.DTEND, endMillis);
+        values.put(CalendarContract.Events.HAS_ALARM, 1);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
+        Uri event = cr.insert(EVENTS_URI, values);
+
+        // Display event id.
+        Toast.makeText(getApplicationContext(), "Event added", Toast.LENGTH_SHORT).show();
+
+        /** Adding reminder for event added. */
+        Uri REMINDERS_URI = Uri.parse(getCalendarUriBase(true) + "reminders");
+        values = new ContentValues();
+        values.put(CalendarContract.Reminders.EVENT_ID, Long.parseLong(event.getLastPathSegment()));
+        values.put(CalendarContract.Reminders.METHOD, CalendarContract.Reminders.METHOD_ALERT);
+        values.put(CalendarContract.Reminders.MINUTES, 60);
+        cr.insert(REMINDERS_URI, values);
+
+    }
+
+    /** Returns Calendar Base URI, supports both new and old OS. */
+    private String getCalendarUriBase(boolean eventUri) {
+        Uri calendarURI = null;
+        try {
+            if (android.os.Build.VERSION.SDK_INT <= 7) {
+                calendarURI = (eventUri) ? Uri.parse("content://calendar/") : Uri.parse("content://calendar/calendars");
+            } else {
+                calendarURI = (eventUri) ? Uri.parse("content://com.android.calendar/") : Uri
+                        .parse("content://com.android.calendar/calendars");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return calendarURI.toString();
+    }
+
 }
