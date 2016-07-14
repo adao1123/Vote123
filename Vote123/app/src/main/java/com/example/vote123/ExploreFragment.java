58a032ece069cb67191c182cc395f92119c38ce9
adao1123 <adao1193@gmail.com>
2016-07-14 03:31:40 -0700

    Button Functionality

--- Vote123/app/src/main/java/com/example/vote123/ExploreFragment.java
+++ Vote123/app/src/main/java/com/example/vote123/ExploreFragment.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -34,6 +36,15 @@ public class ExploreFragment extends Fragment {
     private ArrayList<String> questionArray;
     private ArrayList<String> costArray;
     private ArrayList<String> proconArray;
+    private TextView questionTV;
+    private Button proconButton;
+    private Button costButton;
+    private Button questionButton;
+    private Button yesButton;
+    private Button maybeButton;
+    private Button noButton;
+    private int currentQuestionNum;
+
 
     public ExploreFragment() {
         // Required empty public constructor
@@ -44,12 +55,21 @@ public class ExploreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        View view =  inflater.inflate(R.layout.fragment_explore, container, false);
+        questionTV = (TextView)view.findViewById(R.id.explore_question_TV_id);
+        proconButton = (Button)view.findViewById(R.id.explore_procon_button_id);
+        proconButton = (Button)view.findViewById(R.id.explore_question_button_id);
+        costButton = (Button)view.findViewById(R.id.explore_cost_button_id);
+        yesButton = (Button)view.findViewById(R.id.explore_yes_button_id);
+        noButton = (Button)view.findViewById(R.id.explore_no_button_id);
+        maybeButton = (Button)view.findViewById(R.id.explore_maybe_button_id);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        currentQuestionNum = 1;
         getSelectedState();
         manageFirebase();
     }
@@ -74,11 +94,84 @@ public class ExploreFragment extends Fragment {
         firebaseQuestions = firebaseState.child("Question");
     }
 
+    private void setButtonListeners(){
+        proconButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayContent(currentQuestionNum,'+');
+            }
+        });
+        costButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayContent(currentQuestionNum,'$');
+            }
+        });
+        questionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayContent(currentQuestionNum,'?');
+            }
+        });
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentQuestionNum++;
+                displayContent(currentQuestionNum,'?');
+            }
+        });
+        maybeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentQuestionNum++;
+                displayContent(currentQuestionNum,'?');
+            }
+        });
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentQuestionNum++;
+                displayContent(currentQuestionNum,'?');
+            }
+        });
+    }
+
     private void addQuestion(){
         firebaseQuestion = firebaseQuestions.child("1");
         firebaseQuestion.setValue("Question1");
         firebaseQuestion.child("Cost").setValue("cost");
         firebaseQuestion.child("Procon").setValue("procon");
+    }
+
+    private void displayContent(int questionNumber,char prompt){
+        firebaseQuestion = firebaseQuestions.child(questionNumber+"");
+        Firebase firebaseTV;
+        switch (prompt){
+            case '$':
+                firebaseTV = firebaseQuestion.child("Cost");
+                break;
+            case '+':
+                firebaseTV = firebaseQuestion.child("Procon");
+                break;
+            case '?':
+                firebaseTV = firebaseQuestion.child("Prompt");
+                break;
+            default:
+                firebaseTV = firebaseQuestion.child("Prompt");
+                break;
+        }
+        firebaseTV.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = (String) dataSnapshot.getValue();
+                questionTV.setText(text);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 
     private void getNumQuestions(){
